25c00331e804e3c5f5263916ae92162e556f4361
luis <luis@soy.yo>
2020-01-15 11:16:39 +0100

    segundo commit antes de crear la base de datos

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/',function(){
     return view('welcome');
 });
+Route::get('/hola',function(){
+    return "Hola Mundo";
+});
+Route::get('/',function($id){
+    return "Mi codigo es: ".$id;
+});
+Route::get('/','StudentController@index')->name('home');
